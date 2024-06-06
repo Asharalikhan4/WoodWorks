6f7a3345723b4097cdf9a9c494589a7f5233c6b4
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-06 12:07:33 +0530

    Tailwind Initialized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function App(): JSX.Element {
     return (
-        <div>
+        <div className="text-4xl">
             <h1>Hello, World!</h1>
         </div>
     );
