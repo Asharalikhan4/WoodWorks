8d513174a9e3ff21f6bd7c91ecf86d164f0cb785
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-06 23:04:24 +0530

    Notification Bar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import NotificationBar from "./components/NotificationBar";
 
 export default function App(): JSX.Element {
     return (
         <div className="text-4xl">
+            <NotificationBar />
             <h1>Hello, World!</h1>
         </div>
     );
---
+++ frontend/src/components/NotificationBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NotificationBar(): JSX.Element {
+    return (
+        <div className="bg-black text-white text-center text-xs md:text-sm font-normal">
+            <h5 className="py-2">
+                Making Your Home Into What You Want.
+            </h5>
+        </div>
+    );
+};
