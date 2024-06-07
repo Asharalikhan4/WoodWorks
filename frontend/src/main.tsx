b665c45a0dae3117933ef65727f55a3cecbe5f67
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-08 01:09:02 +0530

    Signin/Signup api integrated, React-hot-toast for notification added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import router from "./router/Routes";
 
 const container: HTMLElement = document.getElementById("root") as HTMLElement;
@@ -8,5 +9,13 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            toastOptions={{
+                duration: 4000,
+            }}
+        />
     </React.StrictMode>
 );
