c8e3c38b284bca65c0865538ac774b47c24fa79a
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-09 15:15:51 +0530

    Single product get/ update product/ delete product added

--- backend/src/adminControllers/AdminProductControllers.ts
+++ backend/src/adminControllers/AdminProductControllers.ts
@@ -33,3 +33,69 @@ export async function GetAllProducts(req: Request, res: Response) {
         return res.status(500).json({ message: "Internal server error", Error: error });
     };
 };
+
+export async function GetSingleProduct(req: Request, res: Response) {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        };
+        return res.status(200).json({ Product: product });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error", Error: error });
+    };
+};
+
+export async function UpdateProduct(req: Request, res: Response) {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        };
+        const {available, title, img, description, inStock, price, avaliableQuantity, flashSale, ratings} = req.body;
+        if (available) {
+            product.available = available;
+        };
+        if (title) {
+            product.title = title;
+        };
+        if (img) {
+            product.img = img;
+        };
+        if (description) {
+            product.description = description;
+        };
+        if (inStock) {
+            product.inStock = inStock;
+        };
+        if (price) {
+            product.price = price;
+        };
+        if (avaliableQuantity) {
+            product.avaliableQuantity = avaliableQuantity;
+        };
+        if (flashSale) {
+            product.flashSale = flashSale;
+        };
+        if (ratings) {
+            product.ratings = ratings;
+        };
+        await product.save();
+        return res.status(200).json({ message: "Product Updated Successfully", Product: product });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error", Error: error });
+    };
+};
+
+export async function DeleteProduct(req: Request, res: Response) {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        };
+        await product.deleteOne();
+        return res.status(200).json({ message: "Product Deleted Successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error", Error: error });
+    };
+};
--- backend/src/adminRoutes/AdminProductRoutes.ts
+++ backend/src/adminRoutes/AdminProductRoutes.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import { CreateProduct, GetAllProducts } from "../adminControllers/AdminProductControllers.js";
+import { CreateProduct, DeleteProduct, GetAllProducts, GetSingleProduct, UpdateProduct } from "../adminControllers/AdminProductControllers.js";
 const router = express.Router();
 
 router.post("/create-product", CreateProduct);
 router.get("/all-products", GetAllProducts);
+router.get("/single-product/:id", GetSingleProduct);
+router.put("/update-product/:id", UpdateProduct);
+router.delete("/delete-product/:id", DeleteProduct);
 
 export default router;
