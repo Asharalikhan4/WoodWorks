98fbe827f72ff2ef030cd311c287223691b2d7d4
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-10 13:01:06 +0530

    TailwindCss Integrated

--- AdminDashboard/src/App.tsx
+++ AdminDashboard/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function App(): JSX.Element {
     return (
-        <div>
+        <div className="bg-red-400">
             <h1>Admin Dashboard</h1>
         </div>
     );
