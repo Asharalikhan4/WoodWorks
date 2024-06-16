a2217e393d5dec646b56dd2c9a441ce0578a9a3e
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-16 11:57:23 +0530

    Made in india component is responsive

--- frontend/src/components/HomePageComponents/MadeInIndia.tsx
+++ frontend/src/components/HomePageComponents/MadeInIndia.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function MadeInIndia(): JSX.Element {
     return (
-        <div className="lg:text-center lg:space-y-6 lg:my-8">
-            <div className="lg:text-[#121212] lg:text-2xl">MADE IN INDIA</div>
-            <div className="lg: text-[#121212bf] lg:text-base">Dedicated to Solid Wood Furniture. We create quality products that embody craftsmanship, enrich lives,<br /> and connect generations.</div>
+        <div className="mobileContainer text-center space-y-2 md:space-y-6 my-6 md:my-8">
+            <div className="text-[#121212] text-xl md:text-2xl">MADE IN INDIA</div>
+            <div className="text-[#121212bf] text-sm md:text-base">Dedicated to Solid Wood Furniture. We create quality products that embody craftsmanship, enrich lives,<br /> and connect generations.</div>
         </div>
     );
 };
