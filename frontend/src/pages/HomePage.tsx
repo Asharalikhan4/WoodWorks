ecc87253ac976eef0653202f70da69513fcb51b1
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-16 15:26:20 +0530

    VocalForLocal added

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import BeInspired from "../components/HomePageComponents/BeInspired";
 import Accordion from "../components/HomePageComponents/Accordion";
 import Hero from "../components/HomePageComponents/Hero";
 import Collections from "../components/HomePageComponents/Collections";
+import VocalForLocal from "../components/HomePageComponents/VocalForLocal";
 
 export default function HomePage(): JSX.Element {
     return (
@@ -19,6 +20,7 @@ export default function HomePage(): JSX.Element {
             <BeInspired />
             <ProductList heading="Marwari Antique Collection" className="bg-[#121212] text-[#ffffff]" />
             <Collections />
+            <VocalForLocal />
             <ProductList heading="Bowls & Boards" className="bg-[#121212] text-[#ffffff]" />
         </div>
     );
