c95482149079e26d3fd1363ace5b400012c05f84
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-16 23:40:34 +0530

    Collection page done

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -24,6 +24,7 @@ export default function HomePage(): JSX.Element {
             <VocalForLocal />
             <ProductList heading="Bowls & Boards" className="bg-[#121212] text-[#ffffff]" />
             <FooterList />
+            <Accordion />
         </div>
     );
 };
