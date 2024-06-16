4449b320f2d3a9ee9c8489cba293f0f9d64cd0bd
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-16 15:04:59 +0530

    Be Inspired is responsive

--- frontend/src/components/HomePageComponents/BeInspired.tsx
+++ frontend/src/components/HomePageComponents/BeInspired.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function BeInspired(): JSX.Element {
     return (
-        <div className="lg:container lg:mx-auto lg:flex my-8">
-            <div className="lg:flex-1 lg:bg-[#F3F3F3] lg:flex lg:flex-col lg:items-center lg:justify-center lg:text-center gap-y-6">
-                <div className="lg:text-[40px] lg:text-[#121212]">BE INSPIRED</div>
-                <div className="lg:text-[#121212bf] lg:text-base">BY OUR MOST COVETED ARTICLES.</div>
-                <button className="bg-black text-white px-4 py-2 mt-4">Shop Now</button>
-            </div>
-            <div className="lg:flex-1">
+        <div className="mobileContainer grid grid-cols-1 md:grid-cols-2 md:container md:mx-auto py-6">
+            <div>
                 <img src="https://www.lakkadhaara.com/cdn/shop/files/New_Project-16.png?v=1688880212&width=1070" className="h-80% w-80%" alt="product image" />
+            </div>
+            <div className="text-center bg-[#F3F3F3] space-y-6 p-10 md:flex md:flex-col md:items-center md:justify-center">
+                <div className="text-3xl text-[#121212] lg:text-[40px]">BE INSPIRED</div>
+                <div className="text-sm text-[#121212bf] lg:text-base">BY OUR MOST COVETED ARTICLES.</div>
+                <button className="bg-black text-white px-6 py-2 mt-4 transition-all duration-300 ease-in-out transform hover:scale-90">Shop Now</button>
             </div>
         </div>
     );
