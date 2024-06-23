09033d263d97cef274d1e3f4003388459fe48391
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-06-24 00:15:06 +0530

    Accordian fixed

--- frontend/src/components/HomePageComponents/Accordion.tsx
+++ frontend/src/components/HomePageComponents/Accordion.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HorizontalLine from "../HorizontalLine";
 import { GiStrongMan, GiRiceCooker, GiProcessor } from "react-icons/gi";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
@@ -6,29 +6,37 @@ import { CiHeart } from "react-icons/ci";
 
 export default function Accordion(): JSX.Element {
 
+    const [openItemId, setOpenItemId] = useState<string | null>(null);
+
+    const toggleItem = (id: string) => {
+        setOpenItemId(prevId => prevId === id ? null : id);
+    };
+
     const AccordionData = [
         {
-            id: 1,
+            id: "1",
+            icon: <GiStrongMan className="text-2xl" />,
             title: "How solid are we?",
             heading: "Let’s just say we are 100% solid WOOD! Nothing but only the finest material for you.",
             description: "We, at Lakkadhaara, take our work very seriously. Only the finest and most reliable go into the making of Lakkadhaara's exclusive products. We deploy the latest technology and craftspeople that have contributed to the art of making furniture which is one of a kind for generations."
         },
         {
-            id: 2,
+            id: "2",
+            icon: <CiHeart className="text-2xl" />,
             title: "Quality, Always",
             heading: "Quality is never an accident.",
             description: "It is always the result of intelligent effort. We spend most of our time and energy for good design and to achieve high quality products. Every single detail from material to technique is thought through with obsessive attention."
         },
         {
-            id: 3,
+            id: "3",
+            icon: <GiRiceCooker className="text-2xl" />,
             title: "Slow & Steady Commerce",
-            heading: "We believe in slow and steady commerce. We are not in a hurry to sell, we are in a hurry to make you happy.",
             description: "Each and every order is created from scratch and we take about 4-6 weeks to ship. If you need it sooner, please get in touch."
         },
         {
-            id: 4,
+            id: "4",
+            icon: <GiProcessor className="text-2xl" />,
             title: "Transparency In Our Process",
-            heading: "We believe in transparency in our process. We want you to know what goes into making your furniture.",
             description: "From mood boards to delivery, we’re proud of our process. We strive to pull back the curtain on everything we do, giving everyone insight into the “why” and “how” of all-things."
         }
     ];
@@ -40,7 +48,7 @@ export default function Accordion(): JSX.Element {
             <div className="mobileContainer text-center md:container md:mx-auto space-y-6">
                 <div className="text-xs">CRAFTED. CURATED. COLLECTED.</div>
                 <HorizontalLine />
-                <div className="flex items-center justify-between cursor-pointer hover:underline" onClick={() => showDescription(!description)}>
+                {/* <div className="flex items-center justify-between cursor-pointer hover:underline" onClick={() => showDescription(!description)}>
                     <div className="flex items-center gap-x-2">
                         <GiStrongMan className="text-2xl" />
                         How solid are we?
@@ -88,7 +96,39 @@ export default function Accordion(): JSX.Element {
                         <MdKeyboardArrowDown className="text-2xl" />
                     </div>
                 </div>
-                <HorizontalLine />
+                <HorizontalLine /> */}
+
+
+                {
+                    AccordionData?.map((data) => {
+                        return (
+                            <div key={data.id}>
+                                <div className="flex items-center justify-between cursor-pointer hover:underline" onClick={() => toggleItem(data.id)}>
+                                    <div className="flex items-center gap-x-2">
+                                        {data.icon}
+                                        {data.title}
+                                    </div>
+                                    <div>
+                                        {
+                                            openItemId === data.id ? <MdKeyboardArrowUp className="text-2xl" /> : <MdKeyboardArrowDown className="text-2xl" />
+                                        }
+                                    </div>
+                                </div>
+                                {
+                                    openItemId === data.id ? (
+                                        <div className="p-4 space-y-4">
+                                            <div className="text-start text-xl md:text-2xl">{data.heading}</div>
+                                            <div className="text-start">{data.description}</div>
+                                        </div>
+                                    ) : (
+                                        <></>
+                                    )
+                                }
+                                <HorizontalLine />
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     );
